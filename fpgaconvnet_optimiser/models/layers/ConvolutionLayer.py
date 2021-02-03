9a04d71514422e62de26d1b0ad3e386eaff58af6
biggsbenjamin <bb2515@ic.ac.uk>
2021-02-03 15:07:21 +0000

    updated the layers to match Layer template

--- fpgaconvnet_optimiser/models/layers/ConvolutionLayer.py
+++ fpgaconvnet_optimiser/models/layers/ConvolutionLayer.py
@@ -13,7 +13,9 @@ from fpgaconvnet_optimiser.models.layers import Layer
 class ConvolutionLayer(Layer):
     def __init__(
             self,
-            dim,
+            rows,
+            cols,
+            channels,
             filters,
             k_size      =3,
             stride      =1,
@@ -26,7 +28,7 @@ class ConvolutionLayer(Layer):
             sa          =0.5,
             sa_out      =0.5
         ):
-        Layer.__init__(self,dim,coarse_in,coarse_out,data_width)
+        Layer.__init__(self, [rows], [cols], [channels], [coarse_in], [coarse_out], data_width)
 
         # update flags
         self.flags['channel_dependant'] = True
@@ -40,25 +42,20 @@ class ConvolutionLayer(Layer):
         self.stride     = stride
         self.groups     = groups
         self.pad        = pad
-        self.pad_top    = pad - (self.rows - k_size + 2*pad) % stride
-        self.pad_right  = pad - (self.cols - k_size + 2*pad) % stride
+        self.pad_top    = pad - (self.rows[0] - k_size + 2*pad) % stride
+        self.pad_right  = pad - (self.cols[0] - k_size + 2*pad) % stride
         self.pad_bottom = pad
         self.pad_left   = pad
         self.fine       = fine
         self.filters    = filters
 
-        dim_out = [
-            self.filters,
-            self.rows_out(),
-            self.cols_out()
-        ]
         # init modules
         self.modules = {
-            "sliding_window" : SlidingWindow(dim, k_size, stride, self.pad_top, self.pad_right, self.pad_bottom, self.pad_left, data_width),
-            "fork"           : Fork(dim_out,k_size,coarse_out),
-            "conv"           : Conv(dim_out,filters,fine,k_size,groups),
-            "accum"          : Accum(dim_out,filters,groups),
-            "glue"           : Glue(dim_out,filters,coarse_in,coarse_out)
+            "sliding_window" : SlidingWindow(rows, cols, channels, k_size, stride, self.pad_top, self.pad_right, self.pad_bottom, self.pad_left, data_width),
+            "fork"           : Fork(self.rows_out(0), self.cols_out(0), self.filters,k_size,coarse_out),
+            "conv"           : Conv(self.rows_out(0), self.cols_out(0), self.filters,filters,fine,k_size,groups),
+            "accum"          : Accum(self.rows_out(0), self.cols_out(0), self.filters,filters,groups),
+            "glue"           : Glue(self.rows_out(0), self.cols_out(0), self.filters,filters,coarse_in,coarse_out)
         }
         self.update()
         #self.load_coef()
@@ -68,10 +65,10 @@ class ConvolutionLayer(Layer):
         self.sa_out = sa_out
 
     def rows_out(self):
-        return int(math.floor((self.rows_in()-self.k_size+2*self.pad)/self.stride)+1)
+        return int(math.floor((self.rows_in(0)-self.k_size+2*self.pad)/self.stride)+1)
 
     def cols_out(self):
-        return int(math.floor((self.cols_in()-self.k_size+2*self.pad)/self.stride)+1)
+        return int(math.floor((self.cols_in(0)-self.k_size+2*self.pad)/self.stride)+1)
 
     def channels_out(self):
         return self.filters
@@ -86,14 +83,14 @@ class ConvolutionLayer(Layer):
     def layer_info(self,parameters,batch_size=1):
         parameters.batch_size   = batch_size
         parameters.buffer_depth = self.buffer_depth
-        parameters.rows_in      = self.rows_in()
-        parameters.cols_in      = self.cols_in()
-        parameters.channels_in  = self.channels_in()
-        parameters.rows_out     = self.rows_out()
-        parameters.cols_out     = self.cols_out()
-        parameters.channels_out = self.channels_out()
-        parameters.coarse_in    = self.coarse_in
-        parameters.coarse_out   = self.coarse_out
+        parameters.rows_in      = self.rows_in(0)
+        parameters.cols_in      = self.cols_in(0)
+        parameters.channels_in  = self.channels_in(0)
+        parameters.rows_out     = self.rows_out(0)
+        parameters.cols_out     = self.cols_out(0)
+        parameters.channels_out = self.channels_out(0)
+        parameters.coarse_in    = self.coarse_in[0]
+        parameters.coarse_out   = self.coarse_out[0]
         parameters.fine         = self.fine
         parameters.filters      = self.filters
         parameters.kernel_size  = self.k_size
@@ -108,32 +105,32 @@ class ConvolutionLayer(Layer):
     ## UPDATE MODULES ##
     def update(self): 
         # sliding window
-        self.modules['sliding_window'].rows     = self.rows_in()
-        self.modules['sliding_window'].cols     = self.cols_in()
-        self.modules['sliding_window'].channels = int(self.channels/self.coarse_in)
+        self.modules['sliding_window'].rows     = self.rows_in(0)
+        self.modules['sliding_window'].cols     = self.cols_in(0)
+        self.modules['sliding_window'].channels = int(self.channels[0]/self.coarse_in[0])
         # fork
-        self.modules['fork'].rows     = self.rows_out()
-        self.modules['fork'].cols     = self.cols_out()
-        self.modules['fork'].channels = int(self.channels/self.coarse_in)
-        self.modules['fork'].coarse   = self.coarse_out
+        self.modules['fork'].rows     = self.rows_out(0)
+        self.modules['fork'].cols     = self.cols_out(0)
+        self.modules['fork'].channels = int(self.channels[0]/self.coarse_in[0])
+        self.modules['fork'].coarse   = self.coarse_out[0]
         # conv
-        self.modules['conv'].rows     = self.rows_out()
-        self.modules['conv'].cols     = self.cols_out()
-        self.modules['conv'].channels = int(self.channels/self.coarse_in)
-        self.modules['conv'].filters  = int(self.filters/(self.coarse_out*self.groups))
+        self.modules['conv'].rows     = self.rows_out(0)
+        self.modules['conv'].cols     = self.cols_out(0)
+        self.modules['conv'].channels = int(self.channels[0]/self.coarse_in[0])
+        self.modules['conv'].filters  = int(self.filters/(self.coarse_out[0]*self.groups))
         self.modules['conv'].fine     = self.fine
         # accum
-        self.modules['accum'].rows     = self.rows_out()
-        self.modules['accum'].cols     = self.cols_out()
-        self.modules['accum'].channels = int(self.channels/(self.coarse_in))
-        self.modules['accum'].filters  = int(self.filters/(self.coarse_out))
+        self.modules['accum'].rows     = self.rows_out(0)
+        self.modules['accum'].cols     = self.cols_out(0)
+        self.modules['accum'].channels = int(self.channels[0]/(self.coarse_in[0]))
+        self.modules['accum'].filters  = int(self.filters/(self.coarse_out[0]))
         self.modules['accum'].groups   = self.groups
         # glue
-        self.modules['glue'].rows       = self.rows_out()
-        self.modules['glue'].cols       = self.cols_out()
+        self.modules['glue'].rows       = self.rows_out(0)
+        self.modules['glue'].cols       = self.cols_out(0)
         self.modules['glue'].filters    = self.filters
-        self.modules['glue'].coarse_in  = self.coarse_in
-        self.modules['glue'].coarse_out = self.coarse_out
+        self.modules['glue'].coarse_in  = self.coarse_in[0]
+        self.modules['glue'].coarse_out = self.coarse_out[0]
 
 
     ### RATES ### 
@@ -144,38 +141,38 @@ class ConvolutionLayer(Layer):
             rates_graph[0,0] = 1
             rates_graph[0,1] = 1
         else:
-            rates_graph[0,0] = self.modules['sliding_window'].rate_in()
-            rates_graph[0,1] = self.modules['sliding_window'].rate_out()
+            rates_graph[0,0] = self.modules['sliding_window'].rate_in(0)
+            rates_graph[0,1] = self.modules['sliding_window'].rate_out(0)
         # fork
-        rates_graph[1,1] = self.modules['fork'].rate_in()
-        rates_graph[1,2] = self.modules['fork'].rate_out()
+        rates_graph[1,1] = self.modules['fork'].rate_in(0)
+        rates_graph[1,2] = self.modules['fork'].rate_out(0)
         # conv
-        rates_graph[2,2] = self.modules['conv'].rate_in()
-        rates_graph[2,3] = self.modules['conv'].rate_out()
+        rates_graph[2,2] = self.modules['conv'].rate_in(0)
+        rates_graph[2,3] = self.modules['conv'].rate_out(0)
         # accum
-        rates_graph[3,3] = self.modules['accum'].rate_in()
-        rates_graph[3,4] = self.modules['accum'].rate_out()
+        rates_graph[3,3] = self.modules['accum'].rate_in(0)
+        rates_graph[3,4] = self.modules['accum'].rate_out(0)
         # glue 
-        rates_graph[4,4] = self.modules['glue'].rate_in()
-        rates_graph[4,5] = self.modules['glue'].rate_out()
+        rates_graph[4,4] = self.modules['glue'].rate_in(0)
+        rates_graph[4,5] = self.modules['glue'].rate_out(0)
 
         return rates_graph
 
     def get_coarse_in_feasible(self,wr_factor=1):
-        return self.get_factors(int(self.channels_in()/(self.groups*wr_factor)))
+        return self.get_factors(int(self.channels_in(0)/(self.groups*wr_factor)))
 
     def get_coarse_out_feasible(self,wr_factor=1):
-        return self.get_factors(int(self.channels_out()/(self.groups*wr_factor)))
+        return self.get_factors(int(self.channels_out(0)/(self.groups*wr_factor)))
 
     def get_fine_feasible(self):
         #return self.get_factors(int(self.k_size*self.k_size))
         return [ 1, self.k_size, self.k_size*self.k_size ]
 
     def get_weights_reloading_feasible(self):
-        return self.get_factors(int(self.filters/(self.groups*self.coarse_out)))
+        return self.get_factors(int(self.filters/(self.groups*self.coarse_out[0])))
 
     def get_parameters_size(self):
-        weights_size = self.channels * int( self.filters / self.groups ) * self.k_size * self.k_size
+        weights_size = self.channels[0] * int( self.filters / self.groups ) * self.k_size * self.k_size
         bias_size = 0
         return {
             "weights"   : weights_size,
@@ -192,85 +189,85 @@ class ConvolutionLayer(Layer):
 
         if self.k_size == 1:
             sw_rsc      = {"LUT" : 0,"BRAM" : 0,"DSP" : 0,"FF" : 0}
-        if self.coarse_out == 1:
+        if self.coarse_out[0] == 1:
             fork_rsc    = {"LUT" : 0,"BRAM" : 0,"DSP" : 0,"FF" : 0}
-        if int(self.channels/self.coarse_in) == 1:
+        if int(self.channels[0]/self.coarse_in[0]) == 1:
             accum_rsc   = {"LUT" : 0,"BRAM" : 0,"DSP" : 0,"FF" : 0}
-        if self.coarse_in == 1:
+        if self.coarse_in[0] == 1:
             glue_rsc    = {"LUT" : 0,"BRAM" : 0,"DSP" : 0,"FF" : 0}
 
         # weight usage
-        n_filters = float(self.filters*self.channels*self.k_size*self.k_size)/float(self.fine*self.groups*self.coarse_in*self.coarse_out)
-        weights_bram_usage = int(math.ceil((self.weight_width*n_filters)/18000))*self.coarse_in*self.coarse_out*self.fine
+        n_filters = float(self.filters*self.channels[0]*self.k_size*self.k_size)/float(self.fine*self.groups*self.coarse_in[0]*self.coarse_out[0])
+        weights_bram_usage = int(math.ceil((self.weight_width*n_filters)/18000))*self.coarse_in[0]*self.coarse_out[0]*self.fine
 
         # Total
         return {
-            "LUT"  :  sw_rsc['LUT']*self.coarse_in +
-                      fork_rsc['LUT']*self.coarse_in +
-                      conv_rsc['LUT']*self.coarse_in*self.coarse_out +
-                      accum_rsc['LUT']*self.coarse_in*self.coarse_out +
+            "LUT"  :  sw_rsc['LUT']*self.coarse_in[0] +
+                      fork_rsc['LUT']*self.coarse_in[0] +
+                      conv_rsc['LUT']*self.coarse_in[0]*self.coarse_out[0] +
+                      accum_rsc['LUT']*self.coarse_in[0]*self.coarse_out[0] +
                       glue_rsc['LUT'],
-            "FF"   :  sw_rsc['FF']*self.coarse_in +
-                      fork_rsc['FF']*self.coarse_in +
-                      conv_rsc['FF']*self.coarse_in*self.coarse_out +
-                      accum_rsc['FF']*self.coarse_in*self.coarse_out +
+            "FF"   :  sw_rsc['FF']*self.coarse_in[0] +
+                      fork_rsc['FF']*self.coarse_in[0] +
+                      conv_rsc['FF']*self.coarse_in[0]*self.coarse_out[0] +
+                      accum_rsc['FF']*self.coarse_in[0]*self.coarse_out[0] +
                       glue_rsc['FF'],
-            "BRAM" :  sw_rsc['BRAM']*self.coarse_in +
-                      fork_rsc['BRAM']*self.coarse_in +
-                      conv_rsc['BRAM']*self.coarse_in*self.coarse_out +
-                      accum_rsc['BRAM']*self.coarse_out +
+            "BRAM" :  sw_rsc['BRAM']*self.coarse_in[0] +
+                      fork_rsc['BRAM']*self.coarse_in[0] +
+                      conv_rsc['BRAM']*self.coarse_in[0]*self.coarse_out[0] +
+                      accum_rsc['BRAM']*self.coarse_out[0] +
                       glue_rsc['BRAM'] +
                       weights_bram_usage,
-            "DSP" :   sw_rsc['DSP']*self.coarse_in +
-                      fork_rsc['DSP']*self.coarse_in +
-                      conv_rsc['DSP']*self.coarse_in*self.coarse_out +
-                      accum_rsc['DSP']*self.coarse_in*self.coarse_out +
+            "DSP" :   sw_rsc['DSP']*self.coarse_in[0] +
+                      fork_rsc['DSP']*self.coarse_in[0] +
+                      conv_rsc['DSP']*self.coarse_in[0]*self.coarse_out[0] +
+                      accum_rsc['DSP']*self.coarse_in[0]*self.coarse_out[0] +
                       glue_rsc['DSP']
         }
 
     def visualise(self,name):
         cluster = pydot.Cluster(name,label=name)
 
-        for i in range(self.coarse_in):
+        for i in range(self.coarse_in[0]):
             cluster.add_node(pydot.Node( "_".join([name,"sw",str(i)]), label="sw" ))
 
-        for i in range(self.coarse_in):
+        for i in range(self.coarse_in[0]):
             cluster.add_node(pydot.Node( "_".join([name,"fork",str(i)]), label="fork" ))
             cluster.add_edge(pydot.Edge( "_".join([name,"sw",str(i)]) , "_".join([name,"fork",str(i)]) ))
 
-        for i in range(self.coarse_in):
-            for j in range(self.coarse_out):
+        for i in range(self.coarse_in[0]):
+            for j in range(self.coarse_out[0]):
                 cluster.add_node(pydot.Node( "_".join([name,"conv",str(i),str(j)]), label="conv" ))
                 cluster.add_edge(pydot.Edge( "_".join([name,"fork",str(i)]) , "_".join([name,"conv",str(i),str(j)]) ))
 
-        for i in range(self.coarse_in):
-            for j in range(self.coarse_out):
+        for i in range(self.coarse_in[0]):
+            for j in range(self.coarse_out[0]):
                 cluster.add_node(pydot.Node( "_".join([name,"glue",str(j)]), label="+" ))
                 cluster.add_node(pydot.Node( "_".join([name,"accum",str(i),str(j)]), label="accum" ))
                 cluster.add_edge(pydot.Edge( "_".join([name,"conv" ,str(i),str(j)]), "_".join([name,"accum",str(i),str(j)]) ))
                 cluster.add_edge(pydot.Edge( "_".join([name,"accum",str(i),str(j)]), "_".join([name,"glue",str(j)]) ))
 
         # get nodes in and out
-        nodes_in  = [ "_".join([name,"sw",str(i)]) for i in range(self.coarse_in) ]
-        nodes_out = [ "_".join([name,"glue",str(i)]) for i in range(self.coarse_out) ]
+        nodes_in  = [ "_".join([name,"sw",str(i)]) for i in range(self.coarse_in[0]) ]
+        nodes_out = [ "_".join([name,"glue",str(i)]) for i in range(self.coarse_out[0]) ]
 
         return cluster, nodes_in, nodes_out
 
     def functional_model(self,data,weights,bias,batch_size=1):
 
-        assert data.shape[0] == self.rows    , "ERROR (data): invalid row dimension"
-        assert data.shape[1] == self.cols    , "ERROR (data): invalid column dimension"
-        assert data.shape[2] == self.channels, "ERROR (data): invalid channel dimension"
+        assert data.shape[0] == self.rows[0]    , "ERROR (data): invalid row dimension"
+        assert data.shape[1] == self.cols[0]    , "ERROR (data): invalid column dimension"
+        assert data.shape[2] == self.channels[0], "ERROR (data): invalid channel dimension"
 
         assert weights.shape[0] == self.filters , "ERROR (weights): invalid filter dimension"
-        assert weights.shape[1] == int(self.channels/self.groups), "ERROR (weights): invalid channel dimension"
+        assert weights.shape[1] == int(self.channels[0]/self.groups), "ERROR (weights): invalid channel dimension"
         assert weights.shape[2] == self.k_size  , "ERROR (weights): invalid kernel dimension"
         assert weights.shape[3] == self.k_size  , "ERROR (weights): invalid kernel dimension"
 
         assert bias.shape[0] == self.filters  , "ERROR (bias): invalid filter dimension"
 
         # instantiate convolution layer
-        convolution_layer = torch.nn.Conv2d(self.channels, self.filters, self.k_size, 
+        convolution_layer = torch.nn.Conv2d(self.channels[0], self.filters, self.k_size, 
                 stride=self.stride, padding=self.pad, groups=self.groups)
 
         # update weights
--- fpgaconvnet_optimiser/models/layers/PoolingLayer.py
+++ fpgaconvnet_optimiser/models/layers/PoolingLayer.py
@@ -10,7 +10,9 @@ from fpgaconvnet_optimiser.models.layers import Layer
 class PoolingLayer(Layer):
     def __init__(
             self,
-            dim,
+            rows,
+            cols,
+            channels,
             pool_type   ='max',
             k_size      =2,
             stride      =2,
@@ -22,7 +24,7 @@ class PoolingLayer(Layer):
             sa          =0.5,
             sa_out      =0.5
         ):
-        Layer.__init__(self,dim,coarse_in,coarse_out,data_width)
+        Layer.__init__(self, [rows], [cols], [channels], [coarse_in], [coarse_out], data_width)
 
         # update flags
         self.flags['transformable'] = True
@@ -30,8 +32,8 @@ class PoolingLayer(Layer):
         self.k_size     = k_size
         self.stride     = stride
         self.pad        = pad
-        self.pad_top    = pad + (self.rows - k_size + 2*pad) % stride
-        self.pad_right  = pad + (self.cols - k_size + 2*pad) % stride
+        self.pad_top    = pad + (self.rows[0] - k_size + 2*pad) % stride
+        self.pad_right  = pad + (self.cols[0] - k_size + 2*pad) % stride
         self.pad_bottom = pad
         self.pad_left   = pad
         self.fine       = fine
@@ -42,15 +44,15 @@ class PoolingLayer(Layer):
 
         # init modules
         self.modules = {
-            "sliding_window" : SlidingWindow(dim, k_size, stride, self.pad_top, self.pad_right, self.pad_bottom, self.pad_left, data_width),
-            "pool"           : Pool(dim, k_size)
+            "sliding_window" : SlidingWindow(rows, cols, channels, k_size, stride, self.pad_top, self.pad_right, self.pad_bottom, self.pad_left, data_width),
+            "pool"           : Pool(rows, cols, channels, k_size)
         }
         self.update()
         #self.load_coef()
 
         # rows and cols out
-        self.rows_out = lambda : int(math.ceil((self.rows_in()-self.k_size+2*self.pad)/self.stride)+1)
-        self.cols_out = lambda : int(math.ceil((self.cols_in()-self.k_size+2*self.pad)/self.stride)+1)
+        self.rows_out = lambda : int(math.ceil((self.rows_in(0)-self.k_size+2*self.pad)/self.stride)+1)
+        self.cols_out = lambda : int(math.ceil((self.cols_in(0)-self.k_size+2*self.pad)/self.stride)+1)
 
         # rates
         self.rate_in  = lambda i : abs(self.balance_module_rates(self.rates_graph())[0,0])
@@ -64,16 +66,16 @@ class PoolingLayer(Layer):
     def layer_info(self,parameters,batch_size=1):
         parameters.batch_size   = batch_size
         parameters.buffer_depth = self.buffer_depth
-        parameters.rows_in      = self.rows_in()
-        parameters.rows_in      = self.rows_in()
-        parameters.cols_in      = self.cols_in()
-        parameters.channels_in  = self.channels_in()
-        parameters.rows_out     = self.rows_out()
-        parameters.cols_out     = self.cols_out()
-        parameters.channels_out = self.channels_out()
-        parameters.coarse       = self.coarse_in
-        parameters.coarse_in    = self.coarse_in
-        parameters.coarse_out   = self.coarse_out
+        parameters.rows_in      = self.rows_in(0)
+        parameters.rows_in      = self.rows_in(0)
+        parameters.cols_in      = self.cols_in(0)
+        parameters.channels_in  = self.channels_in0()
+        parameters.rows_out     = self.rows_out(0)
+        parameters.cols_out     = self.cols_out(0)
+        parameters.channels_out = self.channels_out(0)
+        parameters.coarse       = self.coarse_in[0]
+        parameters.coarse_in    = self.coarse_in[0]
+        parameters.coarse_out   = self.coarse_out[0]
         parameters.kernel_size  = self.k_size
         parameters.stride       = self.stride
         parameters.pad          = self.pad
@@ -85,23 +87,23 @@ class PoolingLayer(Layer):
     ## UPDATE MODULES ##
     def update(self):
         # sliding window
-        self.modules['sliding_window'].rows     = self.rows_in()
-        self.modules['sliding_window'].cols     = self.cols_in()
-        self.modules['sliding_window'].channels = int(self.channels/self.coarse_in)
+        self.modules['sliding_window'].rows     = self.rows_in(0)
+        self.modules['sliding_window'].cols     = self.cols_in(0)
+        self.modules['sliding_window'].channels = int(self.channels[0]/self.coarse_in[0])
         # pool
-        self.modules['pool'].rows     = self.rows_out()
-        self.modules['pool'].cols     = self.cols_out()
-        self.modules['pool'].channels = int(self.channels/self.coarse_in)
+        self.modules['pool'].rows     = self.rows_out(0)
+        self.modules['pool'].cols     = self.cols_out(0)
+        self.modules['pool'].channels = int(self.channels[0]/self.coarse_in[0])
 
     ### RATES ### TODO
     def rates_graph(self):
         rates_graph = np.zeros( shape=(2,3) , dtype=float )
         # sliding_window
-        rates_graph[0,0] = self.modules['sliding_window'].rate_in()
-        rates_graph[0,1] = self.modules['sliding_window'].rate_out()
+        rates_graph[0,0] = self.modules['sliding_window'].rate_in(0)
+        rates_graph[0,1] = self.modules['sliding_window'].rate_out(0)
         # pool
-        rates_graph[1,1] = self.modules['pool'].rate_in()
-        rates_graph[1,2] = self.modules['pool'].rate_out()
+        rates_graph[1,1] = self.modules['pool'].rate_in(0)
+        rates_graph[1,2] = self.modules['pool'].rate_out(0)
 
         return rates_graph
 
@@ -115,37 +117,37 @@ class PoolingLayer(Layer):
         
         # Total
         return {
-            "LUT"  :  sw_rsc['LUT']*self.coarse_in +
-                      pool_rsc['LUT']*self.coarse_in,
-            "FF"   :  sw_rsc['FF']*self.coarse_in +
-                      pool_rsc['FF']*self.coarse_in,
-            "BRAM" :  sw_rsc['BRAM']*self.coarse_in +
-                      pool_rsc['BRAM']*self.coarse_in,
-            "DSP" :   sw_rsc['DSP']*self.coarse_in +
-                      pool_rsc['DSP']*self.coarse_in
+            "LUT"  :  sw_rsc['LUT']*self.coarse_in[0] +
+                      pool_rsc['LUT']*self.coarse_in[0],
+            "FF"   :  sw_rsc['FF']*self.coarse_in[0] +
+                      pool_rsc['FF']*self.coarse_in[0],
+            "BRAM" :  sw_rsc['BRAM']*self.coarse_in[0] +
+                      pool_rsc['BRAM']*self.coarse_in[0],
+            "DSP" :   sw_rsc['DSP']*self.coarse_in[0] +
+                      pool_rsc['DSP']*self.coarse_in[0]
         }
 
     def visualise(self,name):
         cluster = pydot.Cluster(name,label=name)
 
-        for i in range(self.coarse_in):
+        for i in range(self.coarse_in[0]):
             cluster.add_node(pydot.Node( "_".join([name,"sw",str(i)]), label="sw" ))
 
-        for i in range(self.coarse_out):
+        for i in range(self.coarse_out[0]):
             cluster.add_node(pydot.Node( "_".join([name,"pool",str(i)]), label="pool" ))
             cluster.add_edge(pydot.Edge( "_".join([name,"sw",str(i)]) , "_".join([name,"pool",str(i)]) ))
 
         # get nodes in and out
-        nodes_in  = [ "_".join([name,"sw",str(i)]) for i in range(self.coarse_in) ]
-        nodes_out = [ "_".join([name,"pool",str(i)]) for i in range(self.coarse_out) ]
+        nodes_in  = [ "_".join([name,"sw",str(i)]) for i in range(self.coarse_in[0]) ]
+        nodes_out = [ "_".join([name,"pool",str(i)]) for i in range(self.coarse_out[0]) ]
 
         return cluster, nodes_in, nodes_out
 
     def functional_model(self,data,batch_size=1):
 
-        assert data.shape[0] == self.rows    , "ERROR (data): invalid row dimension"
-        assert data.shape[1] == self.cols    , "ERROR (data): invalid column dimension"
-        assert data.shape[2] == self.channels, "ERROR (data): invalid channel dimension"
+        assert data.shape[0] == self.rows[0]    , "ERROR (data): invalid row dimension"
+        assert data.shape[1] == self.cols[0]    , "ERROR (data): invalid column dimension"
+        assert data.shape[2] == self.channels[0], "ERROR (data): invalid channel dimension"
 
         # instantiate pooling layer
         pooling_layer = torch.nn.MaxPool2d(self.k_size, stride=self.stride, padding=self.pad)
