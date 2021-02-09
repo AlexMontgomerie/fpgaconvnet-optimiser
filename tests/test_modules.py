aad9d7cfe0b458d30a70cf62df6447e488ccc6a3
AlexMontgomerie <am9215@ic.ac.uk>
2021-02-09 13:58:39 +0000

    added all module tests

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -39,6 +39,13 @@ class TestForkModule(TestModuleTemplate,unittest.TestCase):
         "tests/configs/modules/fork/config_0.json",
         "tests/configs/modules/fork/config_1.json",
         "tests/configs/modules/fork/config_2.json",
+        "tests/configs/modules/fork/config_3.json",
+        "tests/configs/modules/fork/config_4.json",
+        "tests/configs/modules/fork/config_5.json",
+        "tests/configs/modules/fork/config_6.json",
+        "tests/configs/modules/fork/config_7.json",
+        "tests/configs/modules/fork/config_8.json",
+        "tests/configs/modules/fork/config_9.json",
     )
     def test_module_configurations(self, config_path):
         # open configuration
@@ -46,7 +53,7 @@ class TestForkModule(TestModuleTemplate,unittest.TestCase):
             config = json.load(f)
 
         # initialise module
-        module = Fork([config["rows"],config["cols"],config["channels"]],
+        module = Fork([config["channels"],config["rows"],config["cols"]],
                 config["kernel_size"],
                 config["coarse"])
 
@@ -54,3 +61,200 @@ class TestForkModule(TestModuleTemplate,unittest.TestCase):
         self.run_test_methods_exist(module)
         self.run_test_dimensions(module)
         self.run_test_rates(module)
+        
+@ddt.ddt
+class TestAccumModule(TestModuleTemplate,unittest.TestCase):
+
+    @ddt.data(
+        "tests/configs/modules/accum/config_0.json",
+        "tests/configs/modules/accum/config_1.json",
+        "tests/configs/modules/accum/config_2.json",
+        "tests/configs/modules/accum/config_3.json",
+        "tests/configs/modules/accum/config_4.json",
+        "tests/configs/modules/accum/config_5.json",
+    )
+    def test_module_configurations(self, config_path):
+        # open configuration
+        with open(config_path, "r") as f:
+            config = json.load(f)
+
+        # initialise module
+        module = Accum([config["channels"],config["rows"],config["cols"]],
+                config["filters"],config["groups"])
+
+        # run tests
+        self.run_test_methods_exist(module)
+        self.run_test_dimensions(module)
+        self.run_test_rates(module)
+        
+        # additional checks
+        self.assertGreater(module.filters,0)
+
+@ddt.ddt
+class TestConvModule(TestModuleTemplate,unittest.TestCase):
+
+    @ddt.data(
+        "tests/configs/modules/conv/config_0.json",
+        "tests/configs/modules/conv/config_1.json",
+        "tests/configs/modules/conv/config_2.json",
+        "tests/configs/modules/conv/config_3.json",
+        "tests/configs/modules/conv/config_4.json",
+        "tests/configs/modules/conv/config_5.json",
+        "tests/configs/modules/conv/config_6.json",
+        "tests/configs/modules/conv/config_7.json",
+        "tests/configs/modules/conv/config_8.json",
+        "tests/configs/modules/conv/config_9.json",
+        "tests/configs/modules/conv/config_10.json",
+    )
+    def test_module_configurations(self, config_path):
+        # open configuration
+        with open(config_path, "r") as f:
+            config = json.load(f)
+
+        # initialise module
+        module = Conv([config["channels"],config["rows"],config["cols"]],
+                config["filters"],config["fine"],config["kernel_size"],config["group"])
+
+        # run tests
+        self.run_test_methods_exist(module)
+        self.run_test_dimensions(module)
+        self.run_test_rates(module)
+
+@ddt.ddt
+class TestGlueModule(TestModuleTemplate,unittest.TestCase):
+
+    @ddt.data(
+        "tests/configs/modules/glue/config_0.json",
+        "tests/configs/modules/glue/config_1.json",
+        "tests/configs/modules/glue/config_2.json",
+        "tests/configs/modules/glue/config_3.json",
+        "tests/configs/modules/glue/config_4.json",
+        "tests/configs/modules/glue/config_5.json",
+        "tests/configs/modules/glue/config_6.json",
+        "tests/configs/modules/glue/config_7.json",
+        "tests/configs/modules/glue/config_8.json",
+        "tests/configs/modules/glue/config_9.json",
+    )
+    def test_module_configurations(self, config_path):
+        # open configuration
+        with open(config_path, "r") as f:
+            config = json.load(f)
+
+        # initialise module
+        module = Glue([config["channels"],config["rows"],config["cols"]],
+                config["filters"],config["coarse_in"],config["coarse_out"])
+
+        # run tests
+        self.run_test_methods_exist(module)
+        self.run_test_dimensions(module)
+        self.run_test_rates(module)
+
+@ddt.ddt
+class TestSlidingWindowModule(TestModuleTemplate,unittest.TestCase):
+    
+    @ddt.data(
+        "tests/configs/modules/sliding_window/config_0.json",
+        "tests/configs/modules/sliding_window/config_1.json",
+        "tests/configs/modules/sliding_window/config_2.json",
+        "tests/configs/modules/sliding_window/config_3.json",
+        "tests/configs/modules/sliding_window/config_4.json",
+        "tests/configs/modules/sliding_window/config_5.json",
+        "tests/configs/modules/sliding_window/config_6.json",
+        "tests/configs/modules/sliding_window/config_7.json",
+    )
+    def test_module_configurations(self, config_path):
+        # open configuration
+        with open(config_path, "r") as f:
+            config = json.load(f)
+
+        # initialise module
+        module = SlidingWindow([config["channels"],config["rows"],config["cols"]],
+                config["kernel_size"],config["stride"],config["pad_top"],
+                config["pad_right"],config["pad_bottom"],config["pad_left"])
+
+        # run tests
+        self.run_test_methods_exist(module)
+        self.run_test_dimensions(module)
+        self.run_test_rates(module)
+
+@ddt.ddt
+class TestPoolModule(TestModuleTemplate,unittest.TestCase):
+
+    @ddt.data(
+        "tests/configs/modules/pool/config_0.json",
+        "tests/configs/modules/pool/config_1.json",
+        "tests/configs/modules/pool/config_2.json",
+        "tests/configs/modules/pool/config_3.json",
+        "tests/configs/modules/pool/config_4.json",
+        "tests/configs/modules/pool/config_5.json",
+        "tests/configs/modules/pool/config_6.json",
+        "tests/configs/modules/pool/config_7.json",
+        "tests/configs/modules/pool/config_8.json",
+        "tests/configs/modules/pool/config_9.json",
+        "tests/configs/modules/pool/config_10.json",
+        "tests/configs/modules/pool/config_11.json",
+    )
+    def test_module_configurations(self, config_path):
+        # open configuration
+        with open(config_path, "r") as f:
+            config = json.load(f)
+
+        # initialise module
+        module = Pool([config["channels"],config["rows"],config["cols"]],
+                config["kernel_size"])
+
+        # run tests
+        self.run_test_methods_exist(module)
+        self.run_test_dimensions(module)
+        self.run_test_rates(module)
+
+@ddt.ddt
+class TestSqueezeModule(TestModuleTemplate,unittest.TestCase):
+
+    @ddt.data(
+        "tests/configs/modules/squeeze/config_0.json",
+        "tests/configs/modules/squeeze/config_1.json",
+    )
+    def test_module_configurations(self, config_path):
+        # open configuration
+        with open(config_path, "r") as f:
+            config = json.load(f)
+
+        # initialise module
+        module = Squeeze([config["channels"],config["rows"],config["cols"]],
+                config["coarse_in"],config["coarse_out"])
+
+        # run tests
+        self.run_test_methods_exist(module)
+        self.run_test_dimensions(module)
+        self.run_test_rates(module)
+
+@ddt.ddt
+class TestReLUModule(TestModuleTemplate,unittest.TestCase):
+
+    @ddt.data(
+        "tests/configs/modules/relu/config_0.json",
+        "tests/configs/modules/relu/config_1.json",
+        "tests/configs/modules/relu/config_2.json",
+        "tests/configs/modules/relu/config_3.json",
+        "tests/configs/modules/relu/config_4.json",
+        "tests/configs/modules/relu/config_5.json",
+        "tests/configs/modules/relu/config_6.json",
+        "tests/configs/modules/relu/config_7.json",
+        "tests/configs/modules/relu/config_8.json",
+        "tests/configs/modules/relu/config_9.json",
+    )
+    def test_module_configurations(self, config_path):
+        # open configuration
+        with open(config_path, "r") as f:
+            config = json.load(f)
+
+        # initialise module
+        module = ReLU([config["channels"],config["rows"],config["cols"]])
+
+        # run tests
+        self.run_test_methods_exist(module)
+        self.run_test_dimensions(module)
+        self.run_test_rates(module)
+
+
