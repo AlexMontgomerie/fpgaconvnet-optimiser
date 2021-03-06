10c264c18d9dd2038e8b8172b60fd7710c2f0f2e
EirikAlbrektsen <52484057+EirikAlbrektsen@users.noreply.github.com>
2021-03-06 10:34:33 +0100

    Add enumeration for communication

--- fpgaconvnet_optimiser/tools/layer_enum.py
+++ fpgaconvnet_optimiser/tools/layer_enum.py
@@ -25,6 +25,7 @@ class LAYER_TYPE(Enum):
     Cast      = 47
     Clip      = 48
     Shape     = 49
+    Communication = 50
     
     @classmethod
     def get_type(cls, t):
@@ -35,13 +36,14 @@ class LAYER_TYPE(Enum):
 
 def to_proto_layer_type(layer_type):
     layer_types = { 
-        LAYER_TYPE.Convolution  : fpgaconvnet_pb2.layer.layer_type.CONVOLUTION, 
-        LAYER_TYPE.InnerProduct : fpgaconvnet_pb2.layer.layer_type.INNER_PRODUCT, 
-        LAYER_TYPE.Pooling      : fpgaconvnet_pb2.layer.layer_type.POOLING, 
-        LAYER_TYPE.ReLU         : fpgaconvnet_pb2.layer.layer_type.RELU, 
-        LAYER_TYPE.Squeeze      : fpgaconvnet_pb2.layer.layer_type.SQUEEZE, 
-        LAYER_TYPE.Concat       : fpgaconvnet_pb2.layer.layer_type.CONCAT, 
-        LAYER_TYPE.BatchNorm    : fpgaconvnet_pb2.layer.layer_type.BATCH_NORM 
+        LAYER_TYPE.Convolution      : fpgaconvnet_pb2.layer.layer_type.CONVOLUTION, 
+        LAYER_TYPE.InnerProduct     : fpgaconvnet_pb2.layer.layer_type.INNER_PRODUCT, 
+        LAYER_TYPE.Pooling          : fpgaconvnet_pb2.layer.layer_type.POOLING, 
+        LAYER_TYPE.ReLU             : fpgaconvnet_pb2.layer.layer_type.RELU, 
+        LAYER_TYPE.Squeeze          : fpgaconvnet_pb2.layer.layer_type.SQUEEZE, 
+        LAYER_TYPE.Concat           : fpgaconvnet_pb2.layer.layer_type.CONCAT, 
+        LAYER_TYPE.BatchNorm        : fpgaconvnet_pb2.layer.layer_type.BATCH_NORM,
+        LAYER_TYPE.Communication    : fpgaconvnet_pb2.layer.layer_type.COMMUNICATION 
     }
     return layer_types.get(layer_type, lambda: "Invalid Layer Type")
 
@@ -54,6 +56,7 @@ def from_proto_layer_type(layer_type):
         fpgaconvnet_pb2.layer.layer_type.SQUEEZE       : LAYER_TYPE.Squeeze,
         fpgaconvnet_pb2.layer.layer_type.CONCAT        : LAYER_TYPE.Concat,
         fpgaconvnet_pb2.layer.layer_type.BATCH_NORM    : LAYER_TYPE.BatchNorm,
+        fpgaconvnet_pb2.layer.layer_type.COMMUNICATION    : LAYER_TYPE.Communication
     }
     return layer_types.get(layer_type, lambda: "Invalid Layer Type")
 
