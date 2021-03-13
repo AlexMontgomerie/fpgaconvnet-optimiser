4baba8ce48e479c28ef41e58f5464a33b4bceb0f
EirikAlbrektsen <52484057+EirikAlbrektsen@users.noreply.github.com>
2021-03-13 10:27:29 +0100

    Fix #24 fix helper functions

--- fpgaconvnet_optimiser/models/layers/CommunicationLayer.py
+++ fpgaconvnet_optimiser/models/layers/CommunicationLayer.py
@@ -12,7 +12,6 @@ class CommunicationLayer(Layer):
     def __init__(
             self,
             dim,
-            filters,
             groups          =1,
             coarse_in       =1,
             coarse_out      =1,
@@ -25,38 +24,23 @@ class CommunicationLayer(Layer):
         # update flags
         self.flags['channel_dependant'] = True
         self.flags['transformable']     = True
-
-        dim_out = [
-            self.filters,
-            self.rows_out(),
-            self.cols_out()
-        ]
         # init modules
         self.modules = {
-            "comm"  : Communication(dim_out,coarse_out,send_nreceive),
+            "comm"  : Communication(dim,coarse_out,send_nreceive),
         }
         self.update()
         #self.load_coef()
-
-        # switching activity
-        self.sa     = sa
-        self.sa_out = sa_out
-
-    def rows_out(self):
-        return int(math.floor((self.rows_in()-self.k_size+2*self.pad)/self.stride)+1)
-
-    def cols_out(self):
-        return int(math.floor((self.cols_in()-self.k_size+2*self.pad)/self.stride)+1)
-
-    def channels_out(self):
-        return self.filters
-
     def rate_in(self,index):
         return abs(self.balance_module_rates(self.rates_graph())[0,0])
 
     def rate_out(self,index):
-        return abs(self.balance_module_rates(self.rates_graph())[4,5])
+        return abs(self.balance_module_rates(self.rates_graph())[0,1])
 
+    def get_coarse_in_feasible(self,wr_factor=1):
+        return self.get_factors(1)
+
+    def get_coarse_out_feasible(self,wr_factor=1):
+        return  self.get_factors(1)
     ## LAYER INFO ##
     def layer_info(self,parameters,batch_size=1):
         parameters.batch_size   = batch_size
@@ -69,16 +53,6 @@ class CommunicationLayer(Layer):
         parameters.channels_out = self.channels_out()
         parameters.coarse_in    = self.coarse_in
         parameters.coarse_out   = self.coarse_out
-        parameters.fine         = self.fine
-        parameters.filters      = self.filters
-        parameters.kernel_size  = self.k_size
-        parameters.stride       = self.stride
-        parameters.groups       = self.groups
-        parameters.pad          = self.pad
-        parameters.pad_top      = self.pad_top
-        parameters.pad_right    = self.pad_right
-        parameters.pad_bottom   = self.pad_bottom
-        parameters.pad_left     = self.pad_left
 
     ## UPDATE MODULES ##
     def update(self): 
@@ -95,27 +69,6 @@ class CommunicationLayer(Layer):
         rates_graph[0,1] = self.modules['comm'].rate_out()
 
         return rates_graph
-
-    def get_coarse_in_feasible(self,wr_factor=1):
-        return self.get_factors(int(self.channels_in()/(self.groups*wr_factor)))
-
-    def get_coarse_out_feasible(self,wr_factor=1):
-        return self.get_factors(int(self.channels_out()/(self.groups*wr_factor)))
-
-    def get_fine_feasible(self):
-        #return self.get_factors(int(self.k_size*self.k_size))
-        return [ 1, self.k_size, self.k_size*self.k_size ]
-
-    def get_weights_reloading_feasible(self):
-        return self.get_factors(int(self.filters/(self.groups*self.coarse_out)))
-
-    def get_parameters_size(self):
-        weights_size = self.channels * int( self.filters / self.groups ) * self.k_size * self.k_size
-        bias_size = 0
-        return {
-            "weights"   : weights_size,
-            "bias"      : bias_size
-        }
 
     def resource(self):
 
