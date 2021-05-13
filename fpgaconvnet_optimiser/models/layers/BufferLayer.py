ff933f5bd4b63c3e8adcdf3e434b0b1fdeb2d25c
biggsbenjamin <bb2515@ic.ac.uk>
2021-05-13 19:18:15 +0100

    added Buffer and BufferLayer for hw optimiser

---
+++ fpgaconvnet_optimiser/models/layers/BufferLayer.py
@@ -0,0 +1,123 @@
+"""
+Buffering layer
+
+Stores intermediate compute information such as results from Conv or Pool layers.
+During DSE the required size will be calculated to store intermediate results at
+branching layers. The position of the buffer layer will then be moved along a
+given branch until the buffer size is feasible and the latency of the exit
+condition is mitigated/matched. For effective pipelining I think.
+
+Secondary function of the buffer is to "drop" a partial calculation.
+Clear a FIFO - takes X number of cycles?
+Drop signal will be control signal from the Exit Condition.
+
+Future goal will be to have buffer as an offchip memory link.
+In this case, the drop might not be used.
+"""
+
+import numpy as np
+import math
+import pydot
+import torch
+
+from fpgaconvnet_optimiser.models.modules import Buffer
+#from fpgaconvnet_optimiser.models.modules import Fork
+from fpgaconvnet_optimiser.models.layers import Layer
+
+class BufferLayer(Layer):
+    def __init__(
+            self,
+            dim,
+            ctrledge,
+            coarse_in   =1,
+            coarse_out  =1,
+            data_width  =16,
+        ):
+        Layer.__init__(self, dim, coarse_in, coarse_out, data_width)
+
+        #ctrledge links to exit condition layer
+        self.ctrledge = ctrledge
+
+        #init modules
+        self.modules = {
+                "buffer"    : Buffer(dim, ctrledge, data_width)
+        }
+        self.update()
+
+    ## LAYER INFO ##
+    def layer_info(self,parameters,batch_size=1):
+        parameters.batch_size   = batch_size
+        parameters.buffer_depth = self.buffer_depth
+        parameters.rows_in      = self.rows_in()
+        parameters.cols_in      = self.cols_in()
+        parameters.channels_in  = self.channels_in()
+        parameters.rows_out     = self.rows_out()
+        parameters.cols_out     = self.cols_out()
+        parameters.channels_out = self.channels_out()
+        parameters.coarse_in    = self.coarse_in
+        parameters.coarse_out   = self.coarse_out
+
+    ## UPDATE MODULES ##
+    def update(self):
+        # sliding window
+        self.modules['buffer'].rows     = self.rows_in()
+        self.modules['buffer'].cols     = self.cols_in()
+        self.modules['buffer'].channels = self.channels_in()
+        #TODO work out if channels = int(self.channels/self.coarse_in)
+
+
+    ### RATES ###
+    def rates_graph(self):
+        rates_graph = np.zeros( shape=(1,2) , dtype=float )
+        #buffer
+            rates_graph[0,0] = self.modules['buffer'].rate_in()
+            rates_graph[0,1] = self.modules['buffer'].rate_out()
+        return rates_graph
+
+    def update_coarse_in(self, coarse_in):
+        self.coarse_in  = coarse_in
+
+    def update_coarse_out(self, coarse_out):
+        self.coarse_out = coarse_out
+
+    #def get_weights_reloading_feasible(self):
+
+    def resource(self):
+
+        buff_rsc    = self.modules['sliding_window'].rsc()
+
+        # Total
+        return {
+            "LUT"  :  buff_rsc['LUT']*self.coarse_in,
+            "FF"   :  buff_rsc['FF']*self.coarse_in,
+            "BRAM" :  buff_rsc['BRAM']*self.coarse_in,
+            "DSP" :   buff_rsc['DSP']*self.coarse_in,
+        }
+
+    def visualise(self,name):
+        cluster = pydot.Cluster(name,label=name)
+
+        for i in range(self.coarse_in):
+            cluster.add_node(pydot.Node( "_".join([name,"buff",str(i)]), label="buff" ))
+
+        # get nodes in and out
+        nodes_in  = [ "_".join([name,"buff",str(i)]) for i in range(self.coarse_in) ]
+        nodes_out = nodes_in
+
+        return cluster, nodes_in, nodes_out
+
+    def functional_model(self,data,weights,bias,batch_size=1):
+        #Buffer is not an ONNX or pytorch op
+        # check input dimensionality
+        assert data.shape[0] == self.rows    , "ERROR: invalid row dimension"
+        assert data.shape[1] == self.cols    , "ERROR: invalid column dimension"
+        assert data.shape[2] == self.channels, "ERROR: invalid channel dimension"
+
+        out = np.zeros((
+            self.rows,
+            self.cols,
+            self.channels),dtype=float)
+
+        if !ctrl_drop: #True means a drop signal has been sent
+            out = data
+        return out
---
+++ fpgaconvnet_optimiser/models/modules/Buffer.py
@@ -0,0 +1,98 @@
+"""
+Buffering Module
+
+Stores intermediate compute information such as results from Conv or Pool layers.
+During DSE the required size will be calculated to store intermediate results at
+branching layers. The position of the buffer layer will then be moved along a
+given branch until the buffer size is feasible and the latency of the exit
+condition is mitigated/matched. For effective pipelining I think.
+
+Secondary function of the buffer is to "drop" a partial calculation.
+Clear a FIFO - takes X number of cycles?
+Drop signal will be control signal from the Exit Condition.
+
+Future goal will be to have buffer as an offchip memory link.
+In this case, the drop might not be used.
+"""
+
+from fpgaconvnet_optimiser.models.modules import Module
+import numpy as np
+import math
+import os
+
+class Buffer(Module):
+    def __init__(
+            self,
+            dim,
+            ctrledge,
+            #filters,
+            #groups,
+            data_width=16
+        ):
+        # init module
+        Module.__init__(self,dim,data_width)
+
+        # init variables
+        self.ctrledge = ctrledge
+        #self.filters = filters
+        #self.groups  = groups
+
+        # load resource coefficients
+        #TODO resource coefficients file for buffer module
+        #self.rsc_coef = np.load(os.path.join(os.path.dirname(__file__),
+        #    "../../coefficients/buffer_rsc_coef.npy"))
+
+    def module_info(self):
+        return {
+            'type'          : self.__class__.__name__.upper(),
+            'rows'          : self.rows_in(),
+            'cols'          : self.cols_in(),
+            'groups'        : self.groups,
+            'channels'      : self.channels_in(),
+            'rows_out'      : self.rows_out(),
+            'cols_out'      : self.cols_out(),
+            'channels_out'  : self.channels_out()
+        }
+
+    def utilisation_model(self):
+        #TODO work out what this should be
+        #how should the FIFOs be laid out?
+        return [
+            1,
+            self.data_width,
+            self.data_width*self.channels
+        ]
+
+    def pipeline_depth(self):
+        #TODO work out if this module can be/needs pipelining
+        return 0
+
+
+    def rsc(self):
+        #basic version is just a single FIFO matching input dims
+        bram_buffer_size =  self.rows*self.cols*self.channels*self.data_width
+
+        bram_buffer = 0
+        if bram_buffer_size >= 512: #taken from Accum.py modules
+            bram_buffer = math.ceil( (bram_acc_buffer_size)/18000)
+        return {
+          "LUT"  : 0, #int(np.dot(self.utilisation_model(), self.rsc_coef[0])),
+          "BRAM" : bram_buffer,
+          "DSP"  : 0,
+          "FF"   : 0 #int(np.dot(self.utilisation_model(), self.rsc_coef[3])),
+        }
+
+    def functional_model(self, data, ctrl_drop):
+        # check input dimensionality
+        assert data.shape[0] == self.rows    , "ERROR: invalid row dimension"
+        assert data.shape[1] == self.cols    , "ERROR: invalid column dimension"
+        assert data.shape[2] == self.channels, "ERROR: invalid channel dimension"
+
+        out = np.zeros((
+            self.rows,
+            self.cols,
+            self.channels),dtype=float)
+
+        if !ctrl_drop: #True means a drop signal has been sent
+            out = data
+        return out
