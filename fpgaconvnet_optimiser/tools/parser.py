d47b261bcfc241f9591761d6d2f79245d9b774ac
biggsbenjamin <bb2515@ic.ac.uk>
2021-05-14 21:19:45 +0100

    updated add_hardware with new layers, linking control signals, fixing graph and ctrl edges

--- fpgaconvnet_optimiser/tools/parser.py
+++ fpgaconvnet_optimiser/tools/parser.py
@@ -19,9 +19,14 @@ from fpgaconvnet_optimiser.models.layers import PoolingLayer
 from fpgaconvnet_optimiser.models.layers import ReLULayer
 from fpgaconvnet_optimiser.models.layers import LRNLayer
 from fpgaconvnet_optimiser.models.layers import SoftMaxLayer
+#EE layers
+from fpgaconvnet_optimiser.models.layers import BufferLayer
+#from fpgaconvnet_optimiser.models.layers import SplitLayer
+from fpgaconvnet_optimiser.models.layers import ExitConditionLayer
+from fpgaconvnet_optimiser.models.layers import ExitSelectLayer
 
 #from fpgaconvnet_optimiser.tools.layer_enum import LAYER_TYPE
-from layer_enum import LAYER_TYPE
+from layer_enum import LAYER_TYPE #TODO MAKE SURE TO CHANGE BACK
 
 def _layer_type(op_type):
     layer_types = {
@@ -74,7 +79,7 @@ def build_graph(model):
     # graph structure
     graph = nx.DiGraph()
     submodels = [] #links to the subgraphs in If ops
-    ctrledges = [] #the name/ID of the if nodes [[ifnode,then,else]]
+    ctrledges = [] #the name/ID of the if nodes [[ifnode,then,else, cond]]
     edges = [] #dataflow edges
     exitedges = [] #dataflow from exits to parent If op
     #TODO this would be the point to add in branch execution rates
@@ -94,13 +99,12 @@ def build_graph(model):
         #add subgraphs to the network
         if _layer_type(node.op_type) == LAYER_TYPE.If:
             print("IFNODE", name)
-            ifnode = [name, None, None]
+            ifnode = [name, None, None, None]
             #access the subgraphs
             for subgraph in node.attribute:
                 print("attr: ", subgraph.name)
 
                 submodels.append(subgraph) #record link to submodels
-
                 subnode_head = onnx_helper._name(subgraph.g.node[0])
                 if subgraph.name == "then_branch":
                     ifnode[1] = subnode_head
@@ -114,7 +118,6 @@ def build_graph(model):
                 for subnode in subgraph.g.node:
                     subname = onnx_helper._name(subnode)
                     print(subname, ":", subnode.op_type)
-
                     # add sub graph node to graph
                     graph.add_node(subname, type=_layer_type(subnode.op_type), hw=None, inputs={} )
                     last_name=subname
@@ -158,7 +161,7 @@ def build_graph(model):
     for edge in edges:
         graph.add_edge(*edge)
     # return graph
-    return graph, ctrledges, exitedges
+    return submodels, graph, ctrledges, exitedges
 
 def add_split_nodes(graph, ctrledges):
     #adding the split nodes for branching
@@ -170,6 +173,7 @@ def add_split_nodes(graph, ctrledges):
             splitnodes.append((node, successors))
     print("splitnodes:", splitnodes)
 
+    save_nodes = []
     for i,(node,successors) in enumerate(splitnodes):
         splitname = "split_" + str(i)
         graph.add_node(splitname, type=LAYER_TYPE.Split, hw=None, inputs={} )
@@ -177,45 +181,69 @@ def add_split_nodes(graph, ctrledges):
             graph.remove_edge(node, succ)
             graph.add_edge(splitname, succ)
         graph.add_edge(node, splitname)
+        save_nodes.append(splitname)
+    return save_nodes
 
 def add_buffer_nodes(graph, ctrledges):
     #adding buffer nodes to store/buffer compute at branch points
-    buffernodes = []
-    for branch_start in ctrledges: #TODO write this more succintly
-        #then = [1], else = [2]
-        predec = graphs.get_prev_nodes(graph, branch_start[1])
+    def _add_buffer_nodes(graph, ctrledges, node, instance, save_nodes):
+        predec = graphs.get_prev_nodes(graph, node)
         if len(predec) > 1:
             raise Exception("Multiple predecessors not supported")
-        buffernodes.append((predec[0], branch_start[1]))
-
-        predec = graphs.get_prev_nodes(graph, branch_start[2])
-        if len(predec) > 1:
-            raise Exception("Multiple predecessors not supported")
-        buffernodes.append((predec[0], branch_start[2]))
-    print("buffernodes:", buffernodes)
-
-    for i,(predec,node) in enumerate(buffernodes):
-        buffername = "buffer_" + str(i)
+        buffername = "buffer_" + str(instance)
         graph.add_node(buffername, type=LAYER_TYPE.Buffer, hw=None, inputs={} )
-        #for succ in successors:
-        graph.remove_edge(predec, node)
-        graph.add_edge(predec, buffername)
+        #insert buffer layer
+        graph.remove_edge(predec[0], node)
+        graph.add_edge(predec[0], buffername)
         graph.add_edge(buffername, node)
+        #update ctrledge to point to buffer
+        save_nodes.append(buffername)
+        return buffername
 
-def shift_crtledges(graph, ctrledges):
-    #assumption that target layer will be immediate predecessor
-    for node in ctrledges:
+    save_nodes = []
+    i=0
+    for branch_start in ctrledges:
+        #then branch = [1], else branch = [2]
+        buffname = _add_buffer_nodes(graph, ctrledges, branch_start[1], i, save_nodes)
+        branch_start[1] = buffname
+        buffname = _add_buffer_nodes(graph, ctrledges, branch_start[2], i+1, save_nodes)
+        branch_start[2] = buffname
+        i+=2
+    return save_nodes
+
+def update_crtledges(graph, ctrledges):
+    #ASSUMPTION: that target layer will be immediate predecessor
+    for node in ctrledges: #will perform at each If op
         #ctrledges[i][0] is the If node
         predec = graphs.get_prev_nodes(graph, node[0])
         if len(predec) > 1:
             raise Exception("Multiple predecessors not supported")
         if graph.nodes[predec[0]]["type"] not in [LAYER_TYPE.Greater]:
             raise Exception("Other layer types not supported")
-        node[0] = predec[0] #replace with predecessor
+        graph.remove_edge(predec[0], node[0]) #remove dataflow edge
+        node[3] = node[0] #set If op to have ctrl edge
+        node[0] = predec[0] #Replace with predecessor
 
-def add_hardware(model, graph):
+def find_ctrl_origin(graph, ctrledges, node):
+    for ctrl in ctrledges:
+        if node == ctrl[1]:
+            #then branch link so EE
+            return ctrl[0], True
+        elif node == ctrl[2]:
+            #else branch link so not EE
+            return ctrl[0], False
+        elif node == ctrl[3]: #for linking exit selct layer
+            #bit of a hack
+            return ctrl[0], None
+    raise Exception("Node has no control input")
+
+def add_hardware(model, submodels, graph, ctrledges, other_nodes):
     # iterate over nodes in graph
-    for node in model.graph.node:
+    all_nodes = [*model.graph.node]
+    for submodel in submodels:
+        for subnode in submodel.g.node:
+            all_nodes.append(subnode)
+    for node in all_nodes:
         # get node name
         name = onnx_helper._name(node)
         # check if node in graph
@@ -306,6 +334,31 @@ def add_hardware(model, graph):
                 1, # initialise coarse out to 0
             )
             continue
+        #top1 exit criterion layer
+        if graph.nodes[name]['type'] == LAYER_TYPE.Greater:
+            #need to have some idea of the hw layer for EC
+            #add the control edges to the buffers/drop points
+            for ctrl in ctrledges:
+                if name == ctrl[0]:
+                    ctrlout = ctrl[1:]
+            if len(ctrlout) == 0:
+                raise NameError("Control edges not found")
+            print("CTRL OUT", ctrlout)
+            graph.nodes[name]['hw'] = ExitConditionLayer([0,0,0], ctrlout)
+            continue
+        #early exit layer
+        if graph.nodes[name]['type'] == LAYER_TYPE.If:
+            #with two exits it makes sense to pull from the if
+            #will need to generalise assumptions for >2 exits
+            #graph - two dataflow inputs, pick either or on hw level
+            ctrl_origin, _ = find_ctrl_origin(graph, ctrledges, name)
+            graph.nodes[name]['hw'] = ExitSelectLayer([0,0,0], ctrl_origin)
+            continue
+        print(name, node.op_type)
+        raise NameError
+
+    #add hardware for the non-ONNX nodes
+    for name in other_nodes:
         #split layer
         if graph.nodes[name]['type'] == LAYER_TYPE.Split:
             #has input and minimum two outputs
@@ -316,25 +369,13 @@ def add_hardware(model, graph):
             #buffer point to be moved up and down links-depending on exit laten
             #maybe do a size calc here?
             #might need to specify link from EC to here?
-
+            ctrl_origin, EE_flag = find_ctrl_origin(graph, ctrledges, name)
+            #ASSUMPTION: then branch corresponds to EE
+            graph.nodes[name]['hw'] = BufferLayer([0,0,0], ctrl_origin, drop_mode=EE_flag)
             continue
-        #top1 exit criterion layer
-        if graph.nodes[name]['type'] == LAYER_TYPE.Greater:
-            #need to have some idea of the hw layer for EC
-            #softmax, reducemax, greater than threshold
-
-            #add the control edges to the buffers/drop points
-            continue
-        #early exit layer
-        if graph.nodes[name]['type'] == LAYER_TYPE.If:
-            #with two exits it makes sense to pull from the if
-            #will need to generalise assumptions for >2 exits
-
-            #graph - two df inputs, pick either or on hw level
-            continue
-
         print(name, graph.nodes[name]['type'])
         raise NameError
+
 
 def add_dimensions(model, graph):
     # add input dimensions
@@ -349,10 +390,13 @@ def add_dimensions(model, graph):
     # iterate over layers in model
     nodes = list(graph.nodes())
     nodes.remove(input_node)
+    print("node list\n", graph.nodes)
     for node in nodes:
+        print("add_dimensions() node:", node)
         # find previous node
         prev_nodes = graphs.get_prev_nodes(graph, node)
         for prev_node in prev_nodes: # TODO: support parallel networks
+            print("add_dimensions() prev_node:", node)
             # get previous node output dimensions
             dim = onnx_helper._out_dim(model, prev_node)
             # update input dimensions
@@ -366,7 +410,7 @@ def parse_net(filepath,view=True):
     model = onnx_helper.load(filepath)
 
     # get graph
-    graph, ctrledges, exitedges = build_graph(model)
+    submodels, graph, ctrledges, exitedges = build_graph(model)
     print("parse_net(): CTRL EDGES\n", ctrledges)
 
     # remove input node
@@ -394,23 +438,27 @@ def parse_net(filepath,view=True):
     filter_node_types(graph, LAYER_TYPE.ReduceMax)
 
     #add in split and buffer/offchip store layer nodes
-    add_split_nodes(graph, ctrledges)
+    other_nodes = add_split_nodes(graph, ctrledges)
     #print("updated edges-split", graph.edges)
-    add_buffer_nodes(graph, ctrledges)
-    print("updated edges-buffer\n", graph.edges)
+    other_nodes += add_buffer_nodes(graph, ctrledges)
+    #print("updated edges-buffer\n", graph.edges)
+    print("Other nodes",other_nodes)
 
-    #shift control edge from If to Greater (the layer standing in as EC)
-    shift_crtledges(graph, ctrledges)
+    #shift control edge start from If to Greater (the layer standing in as EC)
+    #append the ctrl edge from the Greater to If and remove the data edge
+    update_crtledges(graph, ctrledges)
     print("updated ctrledges", ctrledges)
 
     #determine Early Exit points (Identity operations, edge to exit)
     for eedge in exitedges:
         graph.add_edge(*eedge)
-    print("updated edges-exits\n", graph.edges)
+    #remove pass through node
+    filter_node_types(graph, LAYER_TYPE.Identity)
+    print("updated edges-exits,identity\n", graph.edges)
     #TODO separate softmax layer from other layers in model
 
     # add hardware to graph
-    add_hardware(model, graph)
+    add_hardware(model, submodels, graph, ctrledges, other_nodes)
 
     # add layer dimensions
     add_dimensions(model, graph)
