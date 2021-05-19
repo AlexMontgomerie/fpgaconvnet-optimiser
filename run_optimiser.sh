21e978bb7cb1845abbb157b6674d3d73be0af70c
EirikAlbrektsen <eirik.albrektsen@outlook.com>
2021-05-19 15:29:07 +0200

    New run script

--- run_optimiser.sh
+++ run_optimiser.sh
@@ -1,10 +1,10 @@
 platform_path="examples/platforms/zc706.json"
-batch_size=512
+cluster_root="examples/cluster/"
+batch_size=256
 objective="throughput"
 
 name="lenet"
 model_path="examples/models/lenet.onnx"
-output_path="outputs/lenet"
 
 #name="caffenet"
 #model_path="examples/models/caffenet.onnx"
@@ -17,16 +17,32 @@ output_path="outputs/lenet"
 #name="zfnet"
 #model_path="data/models/onnx/zfnet512.onnx"
 #output_path="outputs/zfnet_test"
+output_root="outputs/lenet/test/"
 
+for test in eight nine ten
+do
+    output_path="$output_root${test}"
+    cluster_path="$cluster_root${test}_fpga.json"
+    for i in 0 1 2 3 4 
+    do
+        for j in 1 2
+        do 
+            run_path="${output_path}/run_$(($j + $i*2))"
+            echo "Looping ... number $(($j + $i*2))" 
 
-mkdir -p $output_path
+            mkdir -p $run_path
 
-python -m run_optimiser -n $name \
-    -m $model_path \
-    -p $platform_path \
-    -o $output_path \
-    -b $batch_size \
-    --objective $objective \
-    --transforms fine weights_reloading coarse partition \
-    --optimiser simulated_annealing \
-    --optimiser_config_path examples/optimiser_example.yml
+            python -m fpgaconvnet_optimiser -n $name \
+                -m $model_path \
+                -p $platform_path \
+                -c $cluster_path \
+                -o $run_path \
+                -b $batch_size \
+                --objective $objective \
+                --transforms fine weights_reloading coarse partition \
+                --optimiser simulated_annealing \
+                --optimiser_config_path examples/quick_run.yml &>/dev/null
+        done
+        wait
+    done
+done
