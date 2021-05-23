f6f4e48afa59c85bd3444f35cab98df899f9a92a
EirikAlbrektsen <eirik.albrektsen@outlook.com>
2021-05-23 14:22:33 +0200

    Add get next_partition function

---
+++ fpgaconvnet_optimiser/models/network/tools.py
@@ -0,0 +1,8 @@
+def get_next_partition(self,partition_id):
+
+    next_partition_id = partition_id + 1
+    if next_partition_id == len(self.partitions):
+        return -1 
+    # TODO: Check if there is a connection between partition and next partition next_cluster_id=self.partitions[partition_id].platform["connections_in"]
+    # print("Cluster {}, Partition {}".format(next_cluster_id,next_partition_id))
+    return next_partition_id
