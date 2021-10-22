a0c25eda1adaedddabe2bf0195a88133aa6be00f
AlexMontgomerie <am9215@ic.ac.uk>
2021-10-22 14:13:59 +0100

    correct BRAM configurations added

--- fpgaconvnet_optimiser/tools/resource_model.py
+++ fpgaconvnet_optimiser/tools/resource_model.py
@@ -1,10 +1,11 @@
 import bisect
 import math
 
-BRAM_CONF_WIDTH={1:16000,2:8000,4:4000,9:2000,18:1000,36:512}
-BRAM_CONF_DEPTH={16000:1,8000:2,4000:4,2000:9,1000:18,512:36}
+# BRAM configurations from https://www.xilinx.com/support/documentation/user_guides/ug473_7Series_Memory_Resources.pdf
+BRAM_CONF_WIDTH={1:16384,2:8192,4:4096,9:2048,18:1024,36:512}
+BRAM_CONF_DEPTH={v: k for k, v in BRAM_CONF_WIDTH.items()}
 
-def bram_stream_resource_model(depth, width):
+def bram_resource_model(depth, width):
     # based on xilinx forum post: https://forums.xilinx.com/t5/High-Level-Synthesis-HLS/BRAM-usage-large-for-FIFO/m-p/1247118
 
     assert width > 0, "width must be greater than zero"
@@ -29,27 +30,11 @@ def bram_stream_resource_model(depth, width):
     # return the ceiling
     return math.ceil(width/bram_width)
 
+def bram_stream_resource_model(depth, width):
+    return bram_resource_model(depth, width)
+
 def bram_memory_resource_model(depth, width):
-    assert width > 0, "width must be greater than zero"
-    assert width <= 36, "width must be less than 36"
-
-    # if there is zero depth, return no BRAM usage
-    if depth == 0:
-        return 0
-
-    # find the closest width from the BRAM configuration
-    if width in  list(BRAM_CONF_WIDTH.keys()):
-        bram_width = width
-    else:
-        bram_width = sorted(list(BRAM_CONF_WIDTH.keys()))[
-                bisect.bisect_right(sorted(list(BRAM_CONF_WIDTH.keys())), width)]
-
-    # get the depth for the bram
-    bram_depth = BRAM_CONF_WIDTH[bram_width]
-
-    # return the ceiling
-    return math.ceil(depth/bram_depth)
-
+    return bram_resource_model(depth, width)
 
 def dsp_multiplier_resource_model(multiplicand_width, multiplier_width, dsp_type="DSP48E1"):
     #https://github.com/Xilinx/finn/blob/4fee6ffd8e13f91314ec9086e9ce9b2ea9de15c7/src/finn/custom_op/fpgadataflow/streamingfclayer_batch.py#L368,
