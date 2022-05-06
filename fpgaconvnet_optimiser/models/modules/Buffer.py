decbb5592b8d7c64fb15cd95cc4b4035554affcb
Ben Biggs <b2515@ic.ac.uk>
2022-05-06 15:09:14 +0100

    updated buffer model and coefficients

--- fpgaconvnet_optimiser/models/modules/Buffer.py
+++ fpgaconvnet_optimiser/models/modules/Buffer.py
@@ -31,25 +31,39 @@ class Buffer(Module):
     def __post_init__(self):
         #NOTE using accum resource model for now
         # load the resource model coefficients
+        #self.rsc_coef["LUT"] = np.load(
+        #        os.path.join(os.path.dirname(__file__),
+        #        "../../coefficients/accum_lut.npy"))
+        #self.rsc_coef["FF"] = np.load(
+        #        os.path.join(os.path.dirname(__file__),
+        #        "../../coefficients/accum_ff.npy"))
+        #self.rsc_coef["BRAM"] = np.load(
+        #        os.path.join(os.path.dirname(__file__),
+        #        "../../coefficients/accum_bram.npy"))
+        #self.rsc_coef["DSP"] = np.load(
+        #        os.path.join(os.path.dirname(__file__),
+        #        "../../coefficients/accum_dsp.npy"))
+        #trying buffer coefs
+        #print("NEW BUFFER COEFFS")
         self.rsc_coef["LUT"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/accum_lut.npy"))
+                "../../coefficients/buffer_lut.npy"))
         self.rsc_coef["FF"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/accum_ff.npy"))
+                "../../coefficients/buffer_ff.npy"))
         self.rsc_coef["BRAM"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/accum_bram.npy"))
+                "../../coefficients/buffer_bram.npy"))
         self.rsc_coef["DSP"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/accum_dsp.npy"))
+                "../../coefficients/buffer_dsp.npy"))
 
     def utilisation_model(self):
         return {
-            "LUT"   : np.array([1,1,self.data_width,self.cols,self.rows,self.channels]),
-            "FF"    : np.array([1,1,self.data_width,self.cols,self.rows,self.channels]),
-            "DSP"   : np.array([1,1,self.data_width,self.cols,self.rows,self.channels]),
-            "BRAM"  : np.array([1,1,self.data_width,self.cols,self.rows,self.channels]),
+            "LUT"   : np.array([self.rows,self.cols,self.channels,self.data_width]),
+            "FF"    : np.array([self.rows,self.cols,self.channels,self.data_width]),
+            "DSP"   : np.array([self.rows,self.cols,self.channels,self.data_width]),
+            "BRAM"  : np.array([self.rows,self.cols,self.channels,self.data_width]),
         }
 
     def module_info(self):
@@ -60,6 +74,21 @@ class Buffer(Module):
         info["drop_mode"] = "true" if self.drop_mode == True else "false"
         # return the info
         return info
+
+    def rsc(self,coef=None):
+        # use module resource coefficients if none are given
+        if coef == None:
+            coef = self.rsc_coef
+        # get the buffer buffer BRAM estimate
+        buf_buffer_bram = bram_memory_resource_model(
+                int(self.rows*self.cols*self.channels), self.data_width)
+        # get the linear model estimation
+        rsc = Module.rsc(self, coef)
+        print("linear model:",rsc['BRAM'], "mem rsc model", buf_buffer_bram)
+        # add the bram estimation
+        rsc["BRAM"] = buf_buffer_bram
+        # return the resource usage
+        return rsc
 
     def functional_model(self, data, ctrl_drop):
         # check input dimensionality
