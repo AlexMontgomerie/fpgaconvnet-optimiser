b337034d805dfc5ce0428372e995b914824e034a
Yu-Zhewen <yuzhewen0108@163.com>
2023-11-29 23:54:57 +0000

    Merge branch 'dev-petros' of https://github.com/AlexMontgomerie/fpgaconvnet-optimiser into dev-petros

--- fpgaconvnet/optimiser/solvers/simulated_annealing.py
+++ fpgaconvnet/optimiser/solvers/simulated_annealing.py
@@ -1,8 +1,10 @@
 import copy
 import math
-import random
-from dataclasses import dataclass
 import pickle
+import random
+import time
+from dataclasses import dataclass
+
 from numpy.random import choice
 from tabulate import tabulate
 
@@ -42,6 +44,7 @@ Randomly chooses a transform and hardware component to change. The change is acc
 
         # Attempt to find a good starting point
         if not start:
+            warmup_loop_start_time = time.perf_counter()
             for i in range(START_LOOP):
                 transform = choice(self.transforms, p=self.transforms_probs)
                 self.apply_transform(transform)
@@ -53,7 +56,7 @@ Randomly chooses a transform and hardware component to change. The change is acc
                     break
                 except AssertionError as error:
                     pass
-
+            self.total_opt_time += (time.perf_counter() - warmup_loop_start_time)
         try:
             self.check_resources()
             self.check_constraints()
@@ -61,11 +64,9 @@ Randomly chooses a transform and hardware component to change. The change is acc
             print(f"ERROR: Exceeds resource usage:\n{error}")
             return False
 
+        cooling_loop_start_time = time.perf_counter()
         # Cooling Loop
         while self.T_min < self.T:
-
-            # update partitions
-            self.update_partitions()
 
             # get the current cost
             cost = self.get_cost()
@@ -75,9 +76,6 @@ Randomly chooses a transform and hardware component to change. The change is acc
 
             # several iterations per cool down
             for _ in range(self.iterations):
-
-                # update partitions
-                self.update_partitions()
 
                 # remove all auxiliary layers
                 for i in range(len(self.net.partitions)):
@@ -106,12 +104,19 @@ Randomly chooses a transform and hardware component to change. The change is acc
             except AssertionError:
                 # revert to previous state
                 self.net.partitions = net_partitions
+                self.update_partitions()
                 continue
 
             # Simulated annealing descision
-            if math.exp(min(0,(cost - self.get_cost())/(self.k*self.T))) < random.uniform(0,1):
-                # revert to previous state
-                self.net.partitions = net_partitions
+            new_cost = self.get_cost()
+            if new_cost < cost:
+                # accept new state
+                pass
+            else:
+                if math.exp(min(0,(cost - new_cost)/(self.k*self.T))) < random.uniform(0,1):
+                    # revert to previous state
+                    self.net.partitions = net_partitions
+                    self.update_partitions()
 
             data = [[f"temperature:",
                      f"{self.T:.6f}",
@@ -128,11 +133,15 @@ Randomly chooses a transform and hardware component to change. The change is acc
 
             if self.wandb_enabled:
                 # wandb logging and checkpoint
-                self.wandb_log(**{"temperature": self.T})
+                self.wandb_log(**{"temperature": self.T,
+                                  "exp": math.exp(min(0,(cost - new_cost)/(self.k*self.T))),
+                                  "delta": cost - new_cost})
                 # self.wandb_checkpoint()
 
             # reduce temperature
             self.T *= self.cool
+
+        self.total_opt_time += (time.perf_counter() - cooling_loop_start_time)
 
         # # store dataframe of
         # # https://docs.wandb.ai/guides/data-vis/log-tables
