bb91dc992beb8de47baa1c66c1e50cf2d60a1be8
Petros Toupas <ptoupas@gmail.com>
2023-12-29 13:28:41 +0200

    fix sweep parameters issue

--- fpgaconvnet/optimiser/sweep_wandb.py
+++ fpgaconvnet/optimiser/sweep_wandb.py
@@ -8,8 +8,11 @@ if __name__ == "__main__":
     with open("examples/sweep/sweep_throughput_unet.toml", "r") as f:
         sweep_config = toml.load(f)
 
+    model_name = sweep_config['model_name']
+    sweep_config.pop('model_name')
+
     sweep_id = wandb.sweep(
-        sweep_config, entity="fpgaconvnet", project=f"fpgaconvnet-{sweep_config['model_name']}-{sweep_config['metric']['name']}")
+        sweep_config, entity="fpgaconvnet", project=f"fpgaconvnet-{model_name}-{sweep_config['metric']['name']}")
 
     wandb.agent(sweep_id, entity="fpgaconvnet",
                 function=fpgaconvnet.optimiser.cli.main)
